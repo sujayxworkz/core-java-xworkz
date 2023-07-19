b47b369e99a9ecf5a4d7777d7a101725bef83ab7
sujayxworkz <sujayhiremath246@gmail.com>
2023-07-19 19:16:21 +0530

    adding the homework file

---
+++ Main.java
@@ -0,0 +1,17 @@
+class Main{
+
+        static int age[] = {25,32,14,81,99};
+		
+		public static void main(String args[]){
+			
+			for(int i=0;i<age.length;i++){
+				System.out.println(age[i]);
+				
+			}
+			
+			
+			
+			
+		}
+
+}
